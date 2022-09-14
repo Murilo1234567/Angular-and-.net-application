091a7e1e055603dc38cc1192292c6d17aecc0e3d
Murilo Sousa Moraes <murilosousamoraes@gmail.com>
2022-09-14 13:08:38 -0300

    feat: Adding interfaces and entities

---
+++ API/Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+namespace Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string? Name { get; set; }
+        public string? Image { get; set; }
+        public string? Description { get; set; }
+        public int Stock { get; set; }
+        public bool Status { get; set; }
+        public decimal Price { get; set; }
+    }
+}
